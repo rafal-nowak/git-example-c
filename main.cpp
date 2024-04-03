5cdec1c8c0d42dbb505d9ebd64a28c5663806734
rafal-nowak <contact@rafal-nowak.com>
2024-04-03 12:59:15 +0200

    added simple menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 
+void menu(void) {
+    printf("\n");
+    printf("0 - Exit\n");
+    printf("1 - Add\n");
+    printf("2 - Subtract\n");
+    printf("3 - Multiply\n");
+    printf("4 - Divide\n");
+    printf("Select an option:\n");
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int option = 0;
+
+    printf("Simple Calc\n");
+
+    do {
+        menu();
+        scanf("%d", &option);
+        switch (option) {
+            case 0:
+                break;
+            case 1:
+                printf("Adding");
+                break;
+            case 2:
+                printf("Subtracting");
+                break;
+            case 3:
+                printf("Multiplying");
+                break;
+            case 4:
+                printf("Dividing");
+                break;
+            default:
+                printf("Choose the right option...\n\n");
+        }
+    } while (option != 0);
+
     return 0;
 }
