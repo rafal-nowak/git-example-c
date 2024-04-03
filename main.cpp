86d6cfdae05a3a99270bd7b84f5060a12b18f17f
rafal-nowak <contact@rafal-nowak.com>
2024-04-03 13:06:01 +0200

    added simple calculations

--- main.cpp
+++ main.cpp
@@ -10,8 +10,18 @@ void menu(void) {
     printf("Select an option:\n");
 }
 
+void enterNumbers(int *number1, int *number2) {
+    printf("Enter first number: ");
+    scanf("%d", number1);
+    printf("Enter second number: ");
+    scanf("%d", number2);
+}
+
 int main() {
     int option = 0;
+    int number1 = 0;
+    int number2 = 0;
+    int result = 0;
 
     printf("Simple Calc\n");
 
@@ -22,16 +32,34 @@ int main() {
             case 0:
                 break;
             case 1:
-                printf("Adding");
+                enterNumbers(&number1, &number2);
+                result = number1 + number2;
+                printf("%d + %d = %d\n", number1, number2, result);
                 break;
             case 2:
-                printf("Subtracting");
+                enterNumbers(&number1, &number2);
+                result = number1 - number2;
+                printf("%d - %d = %d\n", number1, number2, result);
                 break;
             case 3:
-                printf("Multiplying");
+                enterNumbers(&number1, &number2);
+                result = number1 * number2;
+                printf("%d * %d = %d\n", number1, number2, result);
                 break;
             case 4:
-                printf("Dividing");
+                enterNumbers(&number1, &number2);
+                if (number2!= 0) {
+                    result = number1 / number2;
+                    printf("%d / %d = %d\n", number1, number2, result);
+                } else {
+                    if (number1 == 0) {
+                        printf("%d / %d = NaN\n", number1, number2);
+                    } else if (number1 > 0) {
+                        printf("%d / %d = +INF\n", number1, number2);
+                    } else {
+                        printf("%d / %d = -INF\n", number1, number2);
+                    }
+                }
                 break;
             default:
                 printf("Choose the right option...\n\n");
